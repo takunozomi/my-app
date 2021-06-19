6f052dd07253cc55948f9d7e655c330343db68fd
高橋拓希 <h.takahashi@snowrobin.jp>
2021-06-20 08:45:49 +0900

    ヘッダーメニューのルーティング設定

--- src/components/molecules/MenuDrawer.tsx
+++ src/components/molecules/MenuDrawer.tsx
@@ -6,18 +6,27 @@ import { Drawer, DrawerOverlay, DrawerBody, DrawerContent, useDisclosure } from 
 type Props = {
   isOpen: boolean;
   onClose: () => void;
+  onClickSetting: () => void;
+  onClickUserManagement: () => void;
+  onClickHome: () => void;
 };
 
 export const MenuDrawer: VFC<Props> = memo((props) => {
-  const { isOpen, onClose } = props;
+  const { isOpen, onClose, onClickSetting, onClickUserManagement, onClickHome } = props;
   return (
     <Drawer isOpen={isOpen} onClose={onClose} placement="left">
       <DrawerOverlay>
         <DrawerBody>
           <DrawerContent p={0} bg="gray.100">
-            <Button w="100%">TOP</Button>
-            <Button w="100%">ユーザー一覧</Button>
-            <Button w="100%">設定</Button>
+            <Button w="100%" onClick={onClickHome}>
+              TOP
+            </Button>
+            <Button w="100%" onClick={onClickUserManagement}>
+              ユーザー一覧
+            </Button>
+            <Button w="100%" onClick={onClickSetting}>
+              設定
+            </Button>
           </DrawerContent>
         </DrawerBody>
       </DrawerOverlay>
