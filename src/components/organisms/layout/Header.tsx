7767ed3360f3148469aa2862d5f7ab13034a7317
高橋拓希 <h.takahashi@snowrobin.jp>
2021-06-19 20:50:12 +0900

    ハンバーガーメニューのドロワー作成

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,21 +1,36 @@
-import { Flex, Heading, Link, Box, IconButton } from "@chakra-ui/react";
+import { Flex, Heading, Link, Box, IconButton, Drawer, DrawerOverlay, DrawerBody, DrawerContent, useDisclosure } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 import { HamburgerIcon } from "@chakra-ui/icons";
+import { Button } from "@chakra-ui/button";
 export const Header: VFC = memo(() => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
   return (
-    <Flex as="nav" bg="teal.500" color="gray.50" align="center" justify="space-between" padding={{ base: "3", md: "5" }}>
-      <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
-        <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
-          ユーザー管理アプリ
-        </Heading>
+    <div>
+      <Flex as="nav" bg="teal.500" color="gray.50" align="center" justify="space-between" padding={{ base: "3", md: "5" }}>
+        <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
+          <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+            ユーザー管理アプリ
+          </Heading>
+        </Flex>
+        <Flex align="center" fontSize="sm" flexGrow={2} display={{ base: "none", md: "flex" }}>
+          <Box pr={4}>
+            <Link>ユーザー一覧</Link>
+          </Box>
+          <Link>設定</Link>
+        </Flex>
+        <IconButton aria-label="メニューボタン" icon={<HamburgerIcon />} size="sm" variant="unstyled" display={{ base: "block", md: "none" }} onClick={onOpen} />
       </Flex>
-      <Flex align="center" fontSize="sm" flexGrow={2} display={{ base: "none", md: "flex" }}>
-        <Box pr={4}>
-          <Link>ユーザー一覧</Link>
-        </Box>
-        <Link>設定</Link>
-      </Flex>
-      <IconButton aria-label="メニューボタン" icon={<HamburgerIcon />} size="sm" variant="unstyled" display={{ base: "block", md: "none" }} />
-    </Flex>
+      <Drawer isOpen={isOpen} onClose={onClose} placement="left">
+        <DrawerOverlay>
+          <DrawerBody>
+            <DrawerContent p={0}>
+              <Button w="100%">TOP</Button>
+              <Button w="100%">ユーザー一覧</Button>
+              <Button w="100%">設定</Button>
+            </DrawerContent>
+          </DrawerBody>
+        </DrawerOverlay>
+      </Drawer>
+    </div>
   );
 });
