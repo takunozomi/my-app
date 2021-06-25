db93969100bdf51ff65b7c62015bf6756bb465d7
タカハシヒロキ <takahashihiroki@takahashihirokinoMacBook-Air.local>
2021-06-25 09:03:54 +0900

    ログイン画面実装

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,7 +1,18 @@
-import {memo, VFC} from 'react';
+import { Stack, Box, Divider, Flex, Heading, Input } from "@chakra-ui/react";
+import { memo, VFC } from "react";
+import { PrimaryButton } from "../atom/PrimaryButton";
 
 export const Login: VFC = memo(() => {
-    return (
-        <p>ログインページ</p>
-    );
-})
+  return (
+    <Flex justify="center" alignItems="center" height="100vh">
+      <Box bg="white" p={8} shadow="md" borderRadius="md">
+        <Heading as="h2">ユーザ管理アプリ</Heading>
+        <Divider my={4} />
+        <Stack px={6} spacing={4}>
+          <Input placeholder="ユーザーID" />
+          <PrimaryButton>ログイン</PrimaryButton>
+        </Stack>
+      </Box>
+    </Flex>
+  );
+});
